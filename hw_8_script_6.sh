94515300d2864edea326be8bb259d018d284b4c2
little-giraffe1 <prokofeva555@gmail.com>
2025-04-20 00:55:54 +0300

    Update hw_8_script_6.sh

--- hw_8_script_6.sh
+++ hw_8_script_6.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-awk '{if (/[0-9]/) & !/[X-Y]/ & !/[x-y]/} END' {print length($0)} ~/ДЗ8/'6'/fin > ~/ДЗ8/'6'/fout.txt
+awk '{if (/[0-9]/) & !/[X-Y]/ & !/[x-y]/} END' {print length($0)} ~/HW8/'6'/fin > ~/HW8/'6'/fout.txt
